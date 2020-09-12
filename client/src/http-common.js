51e9bab5cff4c524940b02659965b86f993c2556
Thereza Bandoli <tcbandolilegg@gmail.com>
2020-09-12 16:11:35 -0300

    tirando umas imagens padrãoo do react

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://meu_link.herokuapp.com/api',
+  baseURL: 'https://tcbl-igti-desafio-final.herokuapp.com/api',
   headers: {
     'Content-type': 'application/json',
   },
